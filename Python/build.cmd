77786f60271f567881ebc8747ae36e098af0825a
Tomas Guzman <42191764+ParadoxARG@users.noreply.github.com>
2019-01-18 16:25:17 -0300

    15

--- Python/build.cmd
+++ Python/build.cmd
@@ -12,4 +12,3 @@ CALL python index.py ..\recognizers-number\resource-definitions.json
 CALL python index.py ..\recognizers-number-with-unit\resource-definitions.json
 CALL python index.py ..\recognizers-date-time\resource-definitions.json
 CALL python index.py ..\recognizers-sequence\resource-definitions.json
-
