7fe226ed8b688e0858ef99aa03d61db62a6aa40a
Tomas Guzman <42191764+ParadoxARG@users.noreply.github.com>
2019-01-18 16:41:29 -0300

    extract tests to .cmd

---
+++ Python/tests.ci.cmd
@@ -0,0 +1,15 @@
+pip install -e .\libraries\recognizers-text\
+
+pip install -e .\libraries\recognizers-number\
+
+pip install -e .\libraries\recognizers-number-with-unit\
+
+pip install -e .\libraries\recognizers-date-time\
+
+pip install -e .\libraries\recognizers-sequence\
+
+pip install -e .\libraries\recognizers-suite\
+
+pip install -r .\tests\requirements.txt
+
+pytest --tb=line
