beee7934ef7ebd0bc47cb2313c4478c09c197a8a
Tomas Guzman <42191764+ParadoxARG@users.noreply.github.com>
2019-01-21 16:05:55 -0300

    32

--- JavaScript/tests.ci.cmd
+++ JavaScript/tests.ci.cmd
@@ -1,10 +1,5 @@
 ECHO ==============================JAVASCRIPT TESTS START==============================
 
 npm run test
-
-IF %ERRORLEVEL% NEQ 0 (
-	ECHO # JS Test failure(s) found!
-	EXIT /b %ERRORLEVEL%
-)
 	
 ECHO ============================== JAVASCRIPT TESTS END ==============================
