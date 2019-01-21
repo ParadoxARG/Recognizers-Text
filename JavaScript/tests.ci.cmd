88bb6c712e314e7db37ebbcce424ced6722f5fae
Tomas Guzman <42191764+ParadoxARG@users.noreply.github.com>
2019-01-21 15:45:30 -0300

    js new error msg

--- JavaScript/tests.ci.cmd
+++ JavaScript/tests.ci.cmd
@@ -3,7 +3,14 @@ ECHO ==============================JAVASCRIPT TESTS START=======================
 npm run test
 
 :TEST_ERROR
-ECHO Java Test failure(s) found!
+ECHO JavaScript Test failure(s) found!
 EXIT /b 1
+
+
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO # JS Test failure(s) found!
+	EXIT /b %ERRORLEVEL%
+)
 	
 ECHO ============================== JAVASCRIPT TESTS END ==============================
