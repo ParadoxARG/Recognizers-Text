0959bd5ce8ff3884ba5cf3f2eea3a71cbb266a86
Tomas Guzman <42191764+ParadoxARG@users.noreply.github.com>
2019-01-21 10:20:46 -0300

    run js tests

---
+++ Python/build.ci.cmd
@@ -0,0 +1,20 @@
+ECHO ==============================PYHTON BUILD START==============================
+
+pushd libraries\resource-generator
+
+REM Dependencies
+ECHO.
+ECHO # Installing Resource Generator Dependencies
+CALL pip install -r .\requirements.txt
+
+REM Build Resources
+ECHO.
+ECHO # Building Resources
+CALL python index.py ..\recognizers-number\resource-definitions.json
+CALL python index.py ..\recognizers-number-with-unit\resource-definitions.json
+CALL python index.py ..\recognizers-date-time\resource-definitions.json
+CALL python index.py ..\recognizers-sequence\resource-definitions.json
+
+popd
+
+ECHO ============================== PYHTON BUILD END ==============================
